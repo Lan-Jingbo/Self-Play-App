d440c837cdb9d88b90d94061a889bf55b5af8565
Lan-Jingbo <lanjingbo114@163.com>
2023-02-08 17:22:51 +0800

    Add color/UI/ImageView
    Video/Navigation to be done tomorrow

--- app/src/main/java/com/example/warmup/UserAdapter.kt
+++ app/src/main/java/com/example/warmup/UserAdapter.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.warmup.Model.*
+import com.google.android.material.imageview.ShapeableImageView
 
 class UserAdapter(val context: Context, val items: ArrayList<UserModelClass>) :
     RecyclerView.Adapter<UserAdapter.ViewHolder>() {
@@ -22,13 +24,14 @@ class UserAdapter(val context: Context, val items: ArrayList<UserModelClass>) :
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = items.get(position)
-        holder.type.text = item.type
+        /*holder.type.text = item.type
         holder.teaserText.text = item.teaserText
         holder.teaserVideo.text = item.teaserVideo.toString()
         holder.showurl.text = item.showUrl
-        holder.teaserImage.text = item.teaserImage.toString()
         holder.id.text = item.id
-        holder.description.text = item.description
+        holder.description.text = item.description*/
+        //holder.teaserImage. = item.teaserImage?.alternativeImageUrl
+        Glide.with(context).load(item.teaserImage?.alternativeImageUrl).into(holder.teaserImage)
         holder.title.text = item.title
     }
 
@@ -45,13 +48,14 @@ class UserAdapter(val context: Context, val items: ArrayList<UserModelClass>) :
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         // Holds the TextView that will add each item to
-        val type = view.findViewById<TextView>(R.id.type)
-        val teaserText = view.findViewById<TextView>(R.id.teaserText)
-        val teaserVideo = view.findViewById<TextView>(R.id.teaserVideo)
-        val showurl = view.findViewById<TextView>(R.id.showUrl)
-        val teaserImage = view.findViewById<TextView>(R.id.teaserImage)
-        val id = view.findViewById<TextView>(R.id.id)
-        val description = view.findViewById<TextView>(R.id.description)
-        val title = view.findViewById<TextView>(R.id.title)
+//        val type = view.findViewById<TextView>(R.id.type)
+//        val teaserText = view.findViewById<TextView>(R.id.teaserText)
+//        val teaserVideo = view.findViewById<TextView>(R.id.teaserVideo)
+//        val showurl = view.findViewById<TextView>(R.id.showUrl)
+//        val teaserImage = view.findViewById<TextView>(R.id.teaserImage)
+//        val id = view.findViewById<TextView>(R.id.id)
+//        val description = view.findViewById<TextView>(R.id.description)
+        val teaserImage = view.findViewById<ShapeableImageView>(R.id.image_content)
+        val title = view.findViewById<TextView>(R.id.tv_title)
     }
 }
